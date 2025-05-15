3f82abd9e5cdf4088c2dd17f3968712c85358679
scixing <whs11998@163.com>
2025-05-15 14:14:16 +0800

    添加项目文件。

---
+++ BiliReactionRoll/BData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BiliReactionRoll
+{
+
+    public class BData
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public int ttl { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public bool has_more { get; set; }
+        public Item[] items { get; set; }
+        public string offset { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Item
+    {
+        public string action { get; set; }
+        public int attend { get; set; }
+        public string desc { get; set; }
+        public string face { get; set; }
+        public string mid { get; set; }
+        public string name { get; set; }
+    }
+
+}
---
+++ BiliReactionRoll/Program.cs
@@ -0,0 +1,49 @@
+﻿using BiliReactionRoll;
+using System.Collections.Immutable;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+var cookie = Environment.GetEnvironmentVariable("BILI_COOKIE");
+if (!string.IsNullOrEmpty(cookie))
+{
+    client.DefaultRequestHeaders.Add("Cookie", cookie);
+}
+else
+{
+    Console.WriteLine("Cookie 未设置，请设置环境变量 BILI_COOKIE");
+    return;
+}
+
+string requestUri = "https://api.bilibili.com/x/polymer/web-dynamic/v1/detail/reaction?id=1040052976726573072&offset={0}&web_location=333.1369";
+
+var data = (await GetData("")).ToArray();
+
+Console.WriteLine("获取数据完成");
+Console.WriteLine("数据如下：");
+System.Console.WriteLine(string.Join(", ", data));
+
+var result = Random.Shared.GetItems(data, 5);
+
+Console.WriteLine("随机抽取结果如下：");
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+
+async Task<ImmutableArray<string>> GetData(string offset)
+{
+    var response = await client.GetAsync(string.Format(requestUri, offset));
+    response.EnsureSuccessStatusCode();
+    var data = await response.Content.ReadFromJsonAsync<BData>();
+    var names = data.data.items.Where(s => s.action == "赞了").Select(s => s.name);
+    if (!data.data.has_more)
+    {
+        return [.. names];
+    }
+    else
+    {
+        return [.. names, .. await GetData(data.data.offset)];
+    }
+}
